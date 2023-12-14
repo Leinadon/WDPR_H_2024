bf3a5278e58c616a3087d44c778d45b5d68b31be
JasperVorwerk <jasper.vorwerk@ogd.nl>
2023-12-14 11:04:21 +0100

    Main method in Program

--- Program.cs
+++ Program.cs
@@ -4,3 +4,10 @@
 //Database seeden (toevoegen van gegevens) --> Les met ORM --> Slide 28, Modelbuilder.
 //Gebruik maken van Lazyloading --> Microsoft.EntityFrameworkCore.Proxies --> UseLazyLoadingProxies() aanroepen --> Kan veranderd zijn.
 
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("De applicatie wordt nu gestart");
+    }
+}
