173a022a5e92e254c78b024c86e5243efcbe1ead
JasperVorwerk <jasper.vorwerk@ogd.nl>
2023-12-20 16:35:35 +0100

    Changes in User

--- src/User.cs
+++ src/User.cs
@@ -8,30 +8,17 @@ public class User
     public string FirstName {get; set;}
     public string LastName {get; set;}
     public string Zipcode {get; set;}
+    public string HouseNumber {get; set;}
+    public DateTime Birthdate {get; set;}
     public string Email {get; set;}
     public string? TelephoneNumber {get; set;}
     public string SortUser {get;set;}
 
-    //Default constructor
-    public User(){
-        this.ID= 0;
-        this.FirstName ="Default voornaam";
-        this.LastName ="Default achternaam";
-        this.Zipcode = "Default postcode";
-        this.Email = "Default email";
-        this.TelephoneNumber = "Default telefoonnummer";
-        this.SortUser = "Default rol";
-    }
     
     //Constructor met alle kenmerken in de parameters.
-    public User(string firstName, string lastname, string zipcode, string email, string? telephoneNumber, string sortUser)
+    public User()
     {
-        this.FirstName = firstName;
-        this.LastName = lastname;
-        this.Zipcode = zipcode;
-        this.Email = email;
-        this.TelephoneNumber = telephoneNumber;
-        this.SortUser = sortUser; 
+        
     }   
 }
 
