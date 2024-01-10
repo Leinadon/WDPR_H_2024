cd8f06d897835aaf567ca17811d2be2c520f3775
JasperVorwerk <jasper.vorwerk@ogd.nl>
2024-01-10 12:36:34 +0100

    Make Repo, service and controller for DoesResearch

---
+++ Repositories/DoesResearchRepository.cs
@@ -0,0 +1,66 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WPR
+{
+    public interface IDoesResearchRepository
+    {
+        Task<List<DoesResearch>> Get();
+        Task<DoesResearch?> GetByID(int id);
+        Task<DoesResearch> Create(DoesResearch doesResearch);
+        Task Update(int id, DoesResearch doesResearch);
+        Task Delete(int id);
+    }
+
+    public class DoesResearchRepository : IDoesResearchRepository
+    {
+        private readonly WPRDbContext _dbContext;
+
+        public DoesResearchRepository(WPRDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<List<DoesResearch>> Get()
+        {
+            return await _dbContext.DoesResearches.ToListAsync();
+        }
+
+        public async Task<DoesResearch?> GetByID(int id)
+        {
+            return await _dbContext.DoesResearches.FirstOrDefaultAsync(d => d.ID == id);
+        }
+
+        public async Task<DoesResearch> Create(DoesResearch doesResearch)
+        {
+            _dbContext.DoesResearches.Add(doesResearch);
+            await _dbContext.SaveChangesAsync();
+            return doesResearch;
+        }
+
+        public async Task Delete(int id)
+        {
+            DoesResearch? doesResearch = await _dbContext.DoesResearches.FirstOrDefaultAsync(d => d.ID == id);
+            if (doesResearch != null)
+            {
+                _dbContext.DoesResearches.Remove(doesResearch);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task Update(int id, DoesResearch doesResearch)
+        {
+            DoesResearch? oldDoesResearch = await _dbContext.DoesResearches.FindAsync(id);
+            if (oldDoesResearch != null)
+            {
+                _dbContext.Update(doesResearch);
+                await _dbContext.SaveChangesAsync();
+            }
+            else
+            {
+                throw new InvalidOperationException($"DoesResearch with ID {id} not found for update.");
+            }
+        }
+    }
+}
---
+++ Services/DoesResearchService.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WPR
+{
+    public interface IDoesResearchService
+    {
+        Task<List<DoesResearch>> Get();
+        Task<DoesResearch?> GetById(int id);
+        Task<DoesResearch?> Create(DoesResearch doesResearch);
+        Task Update(int id, DoesResearch doesResearch);
+        Task Delete(int id);
+    }
+
+    public class DoesResearchService : IDoesResearchService
+    {
+        private readonly IDoesResearchRepository _doesResearchRepository;
+
+        public DoesResearchService(IDoesResearchRepository doesResearchRepository)
+        {
+            this._doesResearchRepository = doesResearchRepository;
+        }
+
+        public async Task<List<DoesResearch>> Get()
+        {
+            return await _doesResearchRepository.Get();
+        }
+
+        public async Task<DoesResearch?> GetById(int id)
+        {
+            return await _doesResearchRepository.GetByID(id);
+        }
+
+        public async Task<DoesResearch?> Create(DoesResearch doesResearch)
+        {
+            return await _doesResearchRepository.Create(doesResearch);
+        }
+
+        public async Task Delete(int id)
+        {
+            await _doesResearchRepository.Delete(id);
+        }
+
+        public async Task Update(int id, DoesResearch doesResearch)
+        {
+            await _doesResearchRepository.Update(id, doesResearch);
+        }
+    }
+}
