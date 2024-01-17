94ebe2cdecd15d5ec569be4543dc8cc68f7f97d3
JasperVorwerk <jasper.vorwerk@ogd.nl>
2024-01-17 21:32:10 +0100

    ip

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,14 @@ import React from "react";
 import Routes from "./Routes";
 
 import { MsalProvider, AuthenticatedTemplate, useMsal, UnauthenticatedTemplate } from '@azure/msal-react';
-import { msalConfig } from './msalConfig';
-import { AppContent } from './AppContent'; // Your main application component
 
 
 function App() {
 
-      
-        return <Routes />;
-      
-    
+        const { instance } = useMsal();
+        const activeAccount = instance.getActiveAccount();
 
+        return <Routes />;
 }
 
-const pca = new PublicClientApplication(msalConfig);
 export default App;
