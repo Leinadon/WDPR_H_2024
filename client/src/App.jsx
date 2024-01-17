afa4fd380fdb33f77b7b000ea53a71dcad79ee76
JasperVorwerk <jasper.vorwerk@ogd.nl>
2024-01-17 21:46:18 +0100

    changed structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Routes from "./Routes";
 
 import { MsalProvider, AuthenticatedTemplate, useMsal, UnauthenticatedTemplate } from '@azure/msal-react';
-
+import "./styles/App.css";
 
 function App() {
 
