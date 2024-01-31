156c752998e718b13e90aadc381c8fd577c9841f
Leinadon <21137943@student.hhs.nl>
2024-01-31 21:02:46 +0100

    Minior change

--- client/src/Links.jsx
+++ client/src/Links.jsx
@@ -1,19 +1,29 @@
-// In een React-component of ander JavaScript-bestand
-
 const Links = {
-    LinkProfielPagina: class {
-      LinkErvaring = class {
-        Link = "https://localhost:7258/api/users/6";
-      };
-  
-      LinkBedrijf = class {
-        Link = "https://localhost:7258/api/companies/1";
-      };
+  LinkProfielPagina: class {
+    LinkErvaring = class {
+      Link = "https://localhost:7258/api/users/6";
+    };
 
-      LinkBeheerder = class {
-        Link = "https://localhost:7258/api/companies/1"
-      }
+    LinkBedrijf = class {
+      Link = "https://localhost:7258/api/companies/1";
+    };
+
+    LinkBeheerder = class {
+      Link = "https://localhost:7258/api/companies/1"
     }
-  };
+  }
+
+  ,LinkOnderzoekPagina: class {
+    LinkErvaring = class {
+      Link = "";
+    }
+    LinBedrijf = class {
+      Link = "";
+    }
+    LinkBeheerder = class {
+      Link = "";
+    }
+  }
+};
 
 export default Links;
